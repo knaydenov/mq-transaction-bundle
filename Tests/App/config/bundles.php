dabe44fab93dd0fbe6b356e571a3ed7a5b1a306e
Klim Naydenov <knaydenov@gmail.com>
2018-12-01 15:14:55 +0300

    Add dependencies

--- Tests/App/config/bundles.php
+++ Tests/App/config/bundles.php
@@ -2,5 +2,8 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    OldSound\RabbitMqBundle\OldSoundRabbitMqBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Kna\MQTransactionBundle\KnaMQTransactionBundle::class => ['all' => true],
 ];
