124cae045e5b1824031a4f3fcbb534088c97e566
Klim Naydenov <knaydenov@gmail.com>
2018-12-01 22:28:17 +0300

    Add consumer

---
+++ Tests/App/src/Consumer/DefaultConsumer.php
@@ -0,0 +1,19 @@
+<?php
+namespace Kna\MQTransactionBundle\Tests\App\Consumer;
+
+
+use OldSound\RabbitMqBundle\RabbitMq\ConsumerInterface;
+use PhpAmqpLib\Message\AMQPMessage;
+
+class DefaultConsumer implements ConsumerInterface
+{
+
+    /**
+     * @param AMQPMessage $msg The message
+     * @return mixed false to reject and requeue, any other value to acknowledge
+     */
+    public function execute(AMQPMessage $msg)
+    {
+        print $msg->getBody();
+    }
+}
